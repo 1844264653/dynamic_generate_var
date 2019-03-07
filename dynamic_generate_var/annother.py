c3dfdc1c7a2771b819de88427653e06835a57792
mytest <1844264653@qq.com>
2019-03-07 23:35:13 +0800

    动态实现变量动态赋值

---
+++ dynamic_generate_var/annother.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/7 23:10
+# @Author  : 海心
+# @Site    : 
+# @File    : annother.py
+# @Software: PyCharm
+# @descri  : 利用setattr
+
+"""
+setattr(x, y, z)    y  z  是动态的
+"""
